d0fdab87e370083db3960a3ea3bf39ea1f27d0cc
Justin Greet <justin.greet11@gmail.com>
2016-02-19 01:08:42 -0500

    Add babel support.

--- app/components/waveform-helpers/perfect-note-service-compiled.js
+++ app/components/waveform-helpers/perfect-note-service-compiled.js
@@ -1,9 +1,10 @@
+'use strict';
+
 var helperModule = angular.module('pianoPitchDetector.waveform-helpers');
 
-helperModule.service('perfectNoteService', ['utilService',
-  function(utilService) {
-    //Hey there
-  this.getPerfectWaveform = function(keyNum) {
+helperModule.service('perfectNoteService', ['utilService', function (utilService) {
+  //Hey there
+  this.getPerfectWaveform = function (keyNum) {
     var freq = utilService.getFrequencyFromKeyNum(keyNum);
     var samplesPerPeriod = utilService.getSamplesPerPeriod(freq, 44100);
     var bufLen = 8192;
@@ -12,11 +13,13 @@ helperModule.service('perfectNoteService', ['utilService',
     for (var i = 0; i < bufLen; i++) {
       // Need to split 360 degrees into samplesPerPeriod pieces
       var indexInPeriod = i % samplesPerPeriod;
-      var degree = (360 / samplesPerPeriod)*indexInPeriod;
-      var radian = degree * (Math.PI/180);
+      var degree = 360 / samplesPerPeriod * indexInPeriod;
+      var radian = degree * (Math.PI / 180);
       buffer.push(Math.sin(radian));
     }
 
     return buffer;
-  }
+  };
 }]);
+
+//# sourceMappingURL=perfect-note-service-compiled.js.map
