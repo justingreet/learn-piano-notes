a2a18a84ccad8342a825f3214b4c5f2e3c014f80
Justin Greet <justin.greet11@gmail.com>
2016-03-05 23:04:26 -0500

    Animate progress in the sheet music.

--- app/components/note-trainer/note-trainer-directive-compiled.js
+++ app/components/note-trainer/note-trainer-directive-compiled.js
@@ -5,12 +5,16 @@ var _createClass = function () { function defineProperties(target, props) { for 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 noteTrainerModule.directive('noteTrainer', function () {
+  var REFRESH_TIME = 40;
+
   var SPACE_BETWEEN_LINES = 50;
 
   var STAFF_HEIGHT = 200;
   var STAFF_WIDTH = 300;
   var NUM_STAFFS_PER_LINE = 3;
-  var NUM_LINES = 2;
+  var NUM_LINES = 3;
+
+  var timePerStaff = 2000;
 
   var Staff = function () {
     function Staff(top, left, noteNum, ctx) {
@@ -20,6 +24,8 @@ noteTrainerModule.directive('noteTrainer', function () {
       this.left = left;
       this.noteNum = noteNum;
       this.ctx = ctx;
+
+      this.isActive = false;
     }
 
     _createClass(Staff, [{
@@ -27,21 +33,38 @@ noteTrainerModule.directive('noteTrainer', function () {
       value: function noteDetected(detectedNote) {}
     }, {
       key: "enter",
-      value: function enter() {}
+      value: function enter() {
+        this.isActive = true;
+      }
     }, {
       key: "exit",
-      value: function exit() {}
+      value: function exit() {
+        this.isActive = false;
+      }
+    }, {
+      key: "updateTop",
+      value: function updateTop(newTop) {
+        this.top = newTop;
+      }
     }, {
       key: "draw",
       value: function draw() {
         var ctx = this.ctx;
+
+        // Draw the box.
         ctx.beginPath();
         ctx.moveTo(this.left, this.top);
+
+        ctx.lineWidth = this.isActive ? 5 : 2;
+
         ctx.lineTo(this.left + STAFF_WIDTH, this.top);
         ctx.lineTo(this.left + STAFF_WIDTH, this.top + STAFF_HEIGHT);
         ctx.lineTo(this.left, this.top + STAFF_HEIGHT);
         ctx.lineTo(this.left, this.top);
         ctx.stroke();
+
+        // Draw some text.
+        ctx.fillText(this.noteNum, this.left + STAFF_WIDTH * .5, this.top + STAFF_HEIGHT * .5);
       }
     }]);
 
@@ -50,32 +73,65 @@ noteTrainerModule.directive('noteTrainer', function () {
 
   var Line = function () {
     function Line(top, ctx) {
+      var opt_left = arguments.length <= 2 || arguments[2] === undefined ? 0 : arguments[2];
+
       _classCallCheck(this, Line);
 
       var maxNote = 50;
       var minNote = 20;
 
+      this.top = top;
       this.ctx = ctx;
+      this.left = opt_left;
+
+      this.width = STAFF_WIDTH * NUM_STAFFS_PER_LINE;
+
+      this.isActive = false;
+      this.activeStaffIndex = -1;
 
       this.staffs = [];
       for (var i = 0; i < NUM_STAFFS_PER_LINE; i++) {
-        var noteNum = Math.random() * (maxNote - minNote) + minNote;
+        var noteNum = Math.round(Math.random() * (maxNote - minNote) + minNote);
         this.staffs.push(new Staff(top, i * STAFF_WIDTH, noteNum, this.ctx));
       }
     }
 
     _createClass(Line, [{
       key: "enter",
-      value: function enter() {}
+      value: function enter() {
+        this.isActive = true;
+        this.activeStaffIndex = 0;
+        this.staffs[0].enter();
+      }
     }, {
       key: "exit",
-      value: function exit() {}
+      value: function exit() {
+        this.isActive = false;
+        this.staffs[this.staffs.length - 1].exit();
+      }
     }, {
       key: "noteDetected",
       value: function noteDetected() {}
     }, {
+      key: "updateTop",
+      value: function updateTop(newTop) {
+        this.staffs.forEach(function (staff) {
+          staff.updateTop(newTop);
+        });
+
+        this.top = newTop;
+      }
+    }, {
       key: "draw",
-      value: function draw() {
+      value: function draw(progressLineX) {
+        if (this.isActive) {
+          var newActiveStaffIndex = Math.floor(progressLineX / STAFF_WIDTH);
+          if (newActiveStaffIndex !== this.activeStaffIndex) {
+            this.staffs[this.activeStaffIndex].exit();
+            this.staffs[newActiveStaffIndex].enter();
+            this.activeStaffIndex = newActiveStaffIndex;
+          }
+        }
         this.staffs.forEach(function (staff) {
           return staff.draw();
         });
@@ -87,8 +143,6 @@ noteTrainerModule.directive('noteTrainer', function () {
 
   var NoteTrainerController = function () {
     function NoteTrainerController() {
-      var _this = this;
-
       _classCallCheck(this, NoteTrainerController);
 
       this.canvasWidth = NUM_STAFFS_PER_LINE * STAFF_WIDTH;
@@ -101,25 +155,87 @@ noteTrainerModule.directive('noteTrainer', function () {
       for (var i = 0; i < NUM_LINES; i++) {
         this.lines.push(new Line(i * STAFF_HEIGHT + i * SPACE_BETWEEN_LINES, this.ctx));
       }
+      this.lines[0].enter();
 
-      //this.draw();
-      window.setTimeout(function () {
-        return _this.draw();
-      }, 10);
+      this.progressLineX = 0;
+      this.progressLineTime = 0;
+
+      this.start();
     }
 
     _createClass(NoteTrainerController, [{
       key: "start",
-      value: function start() {}
+      value: function start() {
+        var _this = this;
+
+        this.refreshInterval = window.setInterval(function () {
+          return _this.update();
+        }, REFRESH_TIME);
+      }
     }, {
       key: "pause",
-      value: function pause() {}
+      value: function pause() {
+        clearInterval(this.refreshInterval);
+      }
+    }, {
+      key: "update",
+      value: function update() {
+        this.draw();
+
+        // Update the progress line.
+        var curLine = this.lines[0];
+
+        this.progressLineTime += REFRESH_TIME;
+        var percentageOfLineCovered = this.progressLineTime / (NUM_STAFFS_PER_LINE * timePerStaff);
+        if (percentageOfLineCovered >= 1) {
+          // Generate a new line.
+          var lineNum = NUM_LINES - 1;
+          var newLineY = lineNum * STAFF_HEIGHT + lineNum * SPACE_BETWEEN_LINES;
+          var newLine = new Line(newLineY, this.ctx);
+
+          // Remove top line.
+          this.lines[0].exit();
+          this.lines.splice(0, 1);
+
+          // Move all existing lines up.
+          this.lines.forEach(function (line) {
+            line.updateTop(line.top - (STAFF_HEIGHT + SPACE_BETWEEN_LINES));
+          });
+          this.lines[0].enter();
+
+          // Add new line to bottom.
+          this.lines.push(newLine);
+
+          this.progressLineTime = 0;
+          percentageOfLineCovered = 0;
+          this.draw();
+        }
+
+        this.progressLineX = percentageOfLineCovered * curLine.width;
+      }
     }, {
       key: "draw",
       value: function draw() {
+        var _this2 = this;
+
+        var ctx = this.ctx;
+
+        // Clear the canvas.
+        ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+
+        // Draw the sheet music.
         this.lines.forEach(function (line) {
-          return line.draw();
+          return line.draw(_this2.progressLineX);
         });
+
+        // Draw the progress line.
+        var oldStrokeStyle = ctx.strokeStyle;
+        ctx.beginPath();
+        ctx.strokeStyle = '#FF0000';
+        ctx.moveTo(this.progressLineX, 0);
+        ctx.lineTo(this.progressLineX, STAFF_HEIGHT);
+        ctx.stroke();
+        ctx.strokeStyle = oldStrokeStyle;
       }
     }]);
 
--- app/components/note-trainer/note-trainer-directive.js
+++ app/components/note-trainer/note-trainer-directive.js
@@ -1,10 +1,14 @@
 noteTrainerModule.directive('noteTrainer', function () {
+  const REFRESH_TIME = 40;
+
   const SPACE_BETWEEN_LINES = 50;
 
   const STAFF_HEIGHT = 200;
   const STAFF_WIDTH = 300;
   const NUM_STAFFS_PER_LINE = 3;
-  const NUM_LINES = 2;
+  const NUM_LINES = 3;
+
+  var timePerStaff = 2000;
 
   class Staff {
     constructor(top, left, noteNum, ctx) {
@@ -12,6 +16,8 @@ noteTrainerModule.directive('noteTrainer', function () {
       this.left = left;
       this.noteNum = noteNum;
       this.ctx = ctx;
+
+      this.isActive = false;
     }
 
     noteDetected(detectedNote) {
@@ -19,53 +25,95 @@ noteTrainerModule.directive('noteTrainer', function () {
     }
 
     enter() {
-
+      this.isActive = true;
     }
 
     exit() {
+      this.isActive = false;
+    }
 
+    updateTop(newTop) {
+      this.top = newTop;
     }
 
     draw() {
       let ctx = this.ctx;
+
+      // Draw the box.
       ctx.beginPath();
       ctx.moveTo(this.left, this.top);
+
+      ctx.lineWidth = (this.isActive) ?
+          5:
+          2;
+
       ctx.lineTo(this.left + STAFF_WIDTH, this.top);
       ctx.lineTo(this.left + STAFF_WIDTH, this.top + STAFF_HEIGHT);
       ctx.lineTo(this.left, this.top + STAFF_HEIGHT);
       ctx.lineTo(this.left, this.top);
       ctx.stroke();
+
+      // Draw some text.
+      ctx.fillText(this.noteNum,
+          this.left + (STAFF_WIDTH*.5),
+          this.top+(STAFF_HEIGHT*.5));
     }
   }
 
   class Line {
-    constructor(top, ctx) {
+    constructor(top, ctx, opt_left = 0) {
       const maxNote = 50;
       const minNote = 20;
 
+      this.top = top;
       this.ctx = ctx;
+      this.left = opt_left;
+
+      this.width = STAFF_WIDTH*NUM_STAFFS_PER_LINE;
+
+      this.isActive = false;
+      this.activeStaffIndex = -1;
 
       this.staffs = [];
       for (let i = 0; i < NUM_STAFFS_PER_LINE; i++) {
-        var noteNum = Math.random()*(maxNote - minNote) + minNote;
+        var noteNum = Math.round(Math.random()*(maxNote - minNote) + minNote);
         this.staffs.push(new Staff(top, i*STAFF_WIDTH,
             noteNum, this.ctx));
       }
     }
 
     enter() {
-
+      this.isActive = true;
+      this.activeStaffIndex = 0;
+      this.staffs[0].enter();
     }
 
     exit() {
-
+      this.isActive = false;
+      this.staffs[this.staffs.length - 1].exit();
     }
 
     noteDetected() {
 
     }
 
-    draw() {
+    updateTop(newTop) {
+      this.staffs.forEach(staff => {
+        staff.updateTop(newTop);
+      });
+
+      this.top = newTop;
+    }
+
+    draw(progressLineX) {
+      if (this.isActive) {
+        let newActiveStaffIndex = Math.floor(progressLineX / STAFF_WIDTH);
+        if (newActiveStaffIndex !== this.activeStaffIndex) {
+          this.staffs[this.activeStaffIndex].exit();
+          this.staffs[newActiveStaffIndex].enter();
+          this.activeStaffIndex = newActiveStaffIndex;
+        }
+      }
       this.staffs.forEach(staff => staff.draw());
     }
   }
@@ -84,21 +132,76 @@ noteTrainerModule.directive('noteTrainer', function () {
         this.lines.push(
             new Line(i*STAFF_HEIGHT + i*SPACE_BETWEEN_LINES, this.ctx));
       }
+      this.lines[0].enter();
 
-      //this.draw();
-      window.setTimeout(() => this.draw(), 10);
+      this.progressLineX = 0;
+      this.progressLineTime = 0;
+
+      this.start();
     }
 
     start() {
-
+      this.refreshInterval =
+          window.setInterval(() => this.update(), REFRESH_TIME);
     }
 
     pause() {
+      clearInterval(this.refreshInterval);
+    }
 
+    update() {
+      this.draw();
+
+      // Update the progress line.
+      let curLine = this.lines[0];
+
+      this.progressLineTime += REFRESH_TIME;
+      let percentageOfLineCovered =
+          this.progressLineTime / (NUM_STAFFS_PER_LINE*timePerStaff);
+      if (percentageOfLineCovered >= 1) {
+        // Generate a new line.
+        let lineNum = NUM_LINES - 1;
+        let newLineY = lineNum * STAFF_HEIGHT + lineNum * SPACE_BETWEEN_LINES;
+        let newLine = new Line(newLineY, this.ctx);
+
+        // Remove top line.
+        this.lines[0].exit();
+        this.lines.splice(0, 1);
+
+        // Move all existing lines up.
+        this.lines.forEach(line => {
+          line.updateTop(line.top - (STAFF_HEIGHT + SPACE_BETWEEN_LINES));
+        });
+        this.lines[0].enter();
+
+        // Add new line to bottom.
+        this.lines.push(newLine);
+
+        this.progressLineTime = 0;
+        percentageOfLineCovered = 0;
+        this.draw();
+      }
+
+      this.progressLineX = percentageOfLineCovered * curLine.width;
     }
 
     draw() {
-      this.lines.forEach(line => line.draw());
+      let ctx = this.ctx;
+
+      // Clear the canvas.
+      ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+
+      // Draw the sheet music.
+      this.lines.forEach(line => line.draw(this.progressLineX));
+
+      // Draw the progress line.
+      let oldStrokeStyle = ctx.strokeStyle;
+      ctx.beginPath();
+      ctx.strokeStyle = '#FF0000';
+      ctx.moveTo(this.progressLineX, 0);
+      ctx.lineTo(this.progressLineX, STAFF_HEIGHT);
+      ctx.stroke();
+      ctx.strokeStyle = oldStrokeStyle;
     }
   }
 
