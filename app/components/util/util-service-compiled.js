796d9a7a2d1ae352dc2af3ac1e4841ab3dd697e7
Justin Greet <justin.greet11@gmail.com>
2016-03-08 23:36:40 -0500

    Write util to determine if key is sharp or flat.

--- app/components/util/util-service-compiled.js
+++ app/components/util/util-service-compiled.js
@@ -13,7 +13,18 @@ utilModule.service('utilService', function () {
     return Math.round(sampleRate / freq);
   };
 
-  this.keyNumToNoteName;
+  this.isKeyNumFlatOrSharp = function (keyNum) {
+    // Let C2 act as our zero.
+    var rootKeyNum = 16;
+
+    // These key numbers (relative to a zero-indexed zero) are sharps and flats.
+    var sharpsAndFlats = [1, 3, 6, 8, 10];
+
+    var KEYS_PER_OCTAVE = 12;
+    var offsetFromZero = (keyNum - rootKeyNum) % KEYS_PER_OCTAVE;
+
+    return sharpsAndFlats.indexOf(offsetFromZero) > -1;
+  };
 });
 
 //# sourceMappingURL=util-service-compiled.js.map
--- app/components/util/util-service.js
+++ app/components/util/util-service.js
@@ -13,5 +13,16 @@ utilModule.service('utilService', function() {
   };
 
 
-  this.keyNumToNoteName
+  this.isKeyNumFlatOrSharp = function(keyNum) {
+    // Let C2 act as our zero.
+    let rootKeyNum = 16;
+
+    // These key numbers (relative to a zero-indexed zero) are sharps and flats.
+    let sharpsAndFlats = [1,3,6,8,10];
+
+    const KEYS_PER_OCTAVE = 12;
+    let offsetFromZero = (keyNum - rootKeyNum) % KEYS_PER_OCTAVE;
+
+    return sharpsAndFlats.indexOf(offsetFromZero) > -1;
+  }
 });
